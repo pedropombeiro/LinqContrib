dc5521e3ec4eb233081b34e1b781f7a8526056cb
lmbpop0 <pedro.pombeiro@liebherr.com>
2013-03-12 12:21:13 +0100

    First extension methods added.

---
+++ src/Linq.Contrib/LinqExtensions.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LinqExtensions.cs" company="Developer In The Flow">
+//   © 2012-2013 Pedro Pombeiro
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Linq.Contrib
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class LinqExtensions
+    {
+        #region Public Methods and Operators
+
+        public static IEnumerable<IEnumerable<T>> Segment<T>(this IEnumerable<T> source, int segmentSize)
+        {
+            var index = 0;
+
+            for (;;)
+            {
+// ReSharper disable PossibleMultipleEnumeration
+                var sourceSegment = source.Skip(index++ * segmentSize)
+                                          .Take(segmentSize);
+                if (!sourceSegment.Any())
+                {
+                    yield break;
+                }
+
+                yield return sourceSegment;
+
+// ReSharper restore PossibleMultipleEnumeration
+            }
+        }
+
+        public static T SingleOfType<T>(this IEnumerable source)
+        {
+            return source.OfType<T>().Single();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Linq.Contrib/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Developer In The Flow">
+//   © 2012-2013 Pedro Pombeiro
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Linq.Contrib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Developer In The Flow")]
+[assembly: AssemblyProduct("Linq.Contrib")]
+[assembly: AssemblyCopyright("Copyright © Pedro Pombeiro 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("75f3509e-1982-40fd-a9af-fa705c9af2ea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
